ec1d8109a167d7e5ade0b37e3d7f4b0375fbd025
Aghamoghlanov Rustam <raghamoghlanov@gmail.com>
2020-12-04 21:01:08 +0400

    API correction

--- server/serverTools/fbAuth.js
+++ server/serverTools/fbAuth.js
@@ -2,19 +2,22 @@ const jwtDecode = require("jwt-decode");
 const { Int } = require("mssql");
 
 module.exports = (req, res, next) => {
-	let idToken;
-	if (req.headers.authentication && req.headers.authentication.startsWith("Bearer ")) {
-		idToken = req.headers.authentication.split("Bearer ")[1];
-	} else {
-		console.error("No token found");
-		return res.status(403).json({ error: "unauthorized" });
-	}
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else {
+    console.error("No token found");
+    return res.status(403).json({ error: "unauthorized" });
+  }
 
-	const token = jwtDecode(idToken);
-	const data = token.data;
+  const token = jwtDecode(idToken);
+  const data = token.data;
 
-	if (data.id) {
-		return next();
-	}
-	return res.status(403).json({ error: "unauthorized" });
+  if (data.id) {
+    return next();
+  }
+  return res.status(403).json({ error: "unauthorized" });
 };
