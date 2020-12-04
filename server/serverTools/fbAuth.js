e312155762baa086b982233988ed7ead872a266e
Emin Azadov <emin.azadov.03@gmail.com>
2020-12-04 16:15:05 +0400

    Changes

--- server/serverTools/fbAuth.js
+++ server/serverTools/fbAuth.js
@@ -3,8 +3,8 @@ const { Int } = require("mssql");
 
 module.exports = (req, res, next) => {
 	let idToken;
-	if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
-		idToken = req.headers.authorization.split("Bearer ")[1];
+	if (req.headers.authentication && req.headers.authentication.startsWith("Bearer ")) {
+		idToken = req.headers.authentication.split("Bearer ")[1];
 	} else {
 		console.error("No token found");
 		return res.status(403).json({ error: "unauthorized" });
