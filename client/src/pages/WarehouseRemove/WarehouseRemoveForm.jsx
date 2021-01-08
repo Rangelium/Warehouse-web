3a887b3939d4ed5c1e22699831df378e2fde2867
Aghamoghlanov Rustam <raghamoghlanov@gmail.com>
2021-01-08 21:43:13 +0400

    small bug fix

--- client/src/pages/WarehouseRemove/WarehouseRemoveForm.jsx
+++ client/src/pages/WarehouseRemove/WarehouseRemoveForm.jsx
@@ -296,8 +296,9 @@ export default class WarehouseRemoveForm extends Component {
               <h1>
                 Qalıb:
                 <span>
-                  {parseInt(this.props.data[this.state.activeStep].amount) -
-                    this.state.selectedAmounts[this.state.activeStep]}
+                  {parseInt(
+                    this.props.data[this.state.activeStep].amount_left
+                  ) - this.state.selectedAmounts[this.state.activeStep]}
                 </span>
               </h1>
             </div>
